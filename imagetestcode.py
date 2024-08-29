b513a44933cc3ee143a992d23372876e3157126c
Aniket Parasher <107147706+AniketParasher@users.noreply.github.com>
2024-08-29 12:12:11 +0530

    Update imagetestcode.py

--- imagetestcode.py
+++ imagetestcode.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # URL of the image in your GitHub repository
-image_url = "https://raw.githubusercontent.com/your-username/your-repo/main/images/sample_image.png"
+image_url = "https://raw.githubusercontent.com/AniketParasher/pdfcreator/main/cg.png"
 
 # Display the image
 st.image(image_url, caption='Sample Image', use_column_width=True)
